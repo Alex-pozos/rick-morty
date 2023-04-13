5ef693fc72e92d9743cefb861f166eb2994fa90f
Alex-pozos <alejandro.po.ra@gmail.com>
2023-04-12 23:11:19 -0600

    App Rick and Morty

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import logo from './logo.svg';
+import imagenRickMorty from "./img/rick-morty.png";
 import './App.css';
+import { useState } from "react";
+import Characters from "./components/Characters";
 
 function App() {
+
+  const [characters, setCharacters] = useState(null);
+  //Aqui async es una forma de observable para hacer la peticion a la url (API)
+  const reqApi = async () => {
+    // En este punto lo que se puede observar es con await y fetch nos subscribimos para poder hacer la peticion 
+    const api = await fetch('https://rickandmortyapi.com/api/character')
+    // Aqui mapeamos o desfragmentamos lo que nos devolvio en la linea de arriba
+    const characterApi = await api.json();
+    // console.log(characterApi);
+    //Aqui colocamos la informacion de la peticion para corroborar de que tenga algo de informacion
+    setCharacters(characterApi.results);
+  }
+
+  // console.log(characters);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className='title'>Rick & Morty</h1>
+        {characters ? (
+          <Characters characters={characters} setCharacters={setCharacters}></Characters>) 
+          : (
+          <>
+          <img src={imagenRickMorty} alt='Rick & Morty' className='img-home'></img>
+        <button className="btn-search" onClick={reqApi}>Buscar Personajes</button>
+          </>
+          )
+          }
       </header>
     </div>
   );
